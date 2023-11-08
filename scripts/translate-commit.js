2e662021a19346911e068b910ac58c17bfc072b5
Matheus Nunes Boraso <71656999+MatheusNunesBoraso@users.noreply.github.com>
2023-11-08 01:10:26 -0300

    fix: translate script

--- scripts/translate-commit.js
+++ scripts/translate-commit.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-restricted-syntax */
+
 const fs = require("fs");
 const path = require("path");
 
@@ -7,15 +11,43 @@ async function main() {
   const commitMessagePath = path.resolve(".git", "COMMIT_EDITMSG");
   const commitMessage = fs.readFileSync(commitMessagePath, "utf8");
 
-  await translate(String(commitMessage), null, "en")
+  // Lista de palavras-chave a serem evitadas na tradução
+  const keywordsToAvoid = [
+    "feat",
+    "fix",
+    "docs",
+    "style",
+    "refactor",
+    "perf",
+    "test",
+    "build",
+    "ci",
+    "chore",
+    "revert",
+  ];
+
+  // Verifique se a mensagem de commit começa com uma das palavras-chave a serem evitadas
+  let cleanedCommitMessage = commitMessage;
+  let prefix = "";
+  for (const keyword of keywordsToAvoid) {
+    const regex = new RegExp(`^${keyword}:\\s+`, "i");
+    if (regex.test(cleanedCommitMessage)) {
+      prefix = cleanedCommitMessage.match(regex)[0];
+      cleanedCommitMessage = cleanedCommitMessage.replace(regex, "");
+      break;
+    }
+  }
+
+  await translate(cleanedCommitMessage, null, "en")
     .then((res) => {
+      const translatedMessage = prefix + res.translation;
       console.log("");
       console.log("ⓘ   original: ", commitMessage);
-      console.log("✔   translation: ", res.translation);
+      console.log("✔   translation: ", translatedMessage);
       console.log("");
       fs.writeFileSync(
         commitMessagePath,
-        res.translation.toLocaleLowerCase(),
+        translatedMessage.toLocaleLowerCase(),
         "utf8"
       );
     })
